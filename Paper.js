438ff8861fd538ca9e887fbe937578f53099cdd8
aryanRajput270406 <69470195+aryanRajput270406@users.noreply.github.com>
2020-09-10 20:00:21 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,11 @@
+class Paper{
+    constructor(x,y,r){
+    var option={
+        isStatic : false,
+        restitution : 0.3,
+        friction : 0.5,
+        density : 1.2 
+    }
+}
+   Matter;bodies;circle(150, 380, 20)
+}
